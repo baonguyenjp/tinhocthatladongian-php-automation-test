b586e3d2bd8ff2f584bdf571cae55233992e4ca7
Nguyen Quoc Bao <baonguyenjp@gmail.com>
2020-05-02 16:48:50 +0700

    Fix doanh4

--- tests/StaffTest.php
+++ tests/StaffTest.php
@@ -27,7 +27,7 @@ class StaffTest extends TestCase
     public function only_first_name()
     {
         $staff = new Staff;
-        $staff->first_name = "Doanh3";
+        $staff->first_name = "Nguyen";
         $this->assertEquals('Nguyen', $staff->getFullName());
     }
 }
